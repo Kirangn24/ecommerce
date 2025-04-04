733a5d2c847dc5669d5b4c4ab8c800daacb7c186
Kirangn24 <kirannagaraj970@gmail.com>
2025-04-04 15:01:53 +0530

    Updates

---
+++ precache-manifest.a91318bb0074ff4e79b51047411accba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77641845b7f4699f3ea04405da50ac08",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "84225c5f9db9a6ebcd0f",
+    "url": "/ecommerce/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "8183bfa9885213ccee6b",
+    "url": "/ecommerce/static/css/main.10a72923.chunk.css"
+  },
+  {
+    "revision": "84225c5f9db9a6ebcd0f",
+    "url": "/ecommerce/static/js/2.1e795f56.chunk.js"
+  },
+  {
+    "revision": "8183bfa9885213ccee6b",
+    "url": "/ecommerce/static/js/main.9b63d1ff.chunk.js"
+  },
+  {
+    "revision": "e176d1401fcedba87ff8",
+    "url": "/ecommerce/static/js/runtime~main.8409c7fe.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/ecommerce/static/media/logo.9f7e506d.svg"
+  }
+]);
